a393eb0a9d015800504ecd80d5e60522f3bcc8bf
lorenzosco <scotillo.lorenzo.studio@fitstic-edu.com>
2024-04-12 18:03:24 +0200

    add_delete_icon to lists

--- zdor_app/lib/screens/lista_spesa_screen.dart
+++ zdor_app/lib/screens/lista_spesa_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:zdor_app/screens/modifica_lista_screen.dart';
-import 'package:zdor_app/widgets/style/constant.dart';
+import 'package:zdor_app/screens/modifica_lista_screen.dart'; // Importa lo schermo per la modifica della lista
+import 'package:zdor_app/widgets/style/constant.dart'; // Importa le costanti di stile
 
+// Dati JSON iniziali per la Lista della spesa
 final Map<String, dynamic> json = {
   'acquistabili': [
     'Farina',
@@ -22,22 +23,26 @@ final Map<String, dynamic> json = {
   "acquistati": []
 };
 
-// ignore: must_be_immutable
+// Schermo per la visualizzazione e la gestione della lista degli ingredienti
 class IngredientListScreen extends StatefulWidget {
-  List<String> selectedIngredients;
+  final List<String>
+      selectedIngredients; // Elenco degli ingredienti selezionati
 
   IngredientListScreen({Key? key, List<String>? selectedIngredients})
-      : selectedIngredients = selectedIngredients ?? [],
+      : selectedIngredients = selectedIngredients ??
+            [], // Inizializza gli ingredienti selezionati con una lista vuota se non specificato
         super(key: key);
 
   @override
-  _IngredientListScreenState createState() => _IngredientListScreenState();
+  _IngredientListScreenState createState() =>
+      _IngredientListScreenState(); // Crea lo stato associato al widget
 }
 
 class _IngredientListScreenState extends State<IngredientListScreen> {
   List<String> ingredients = List<String>.from(json["acquistabili"]);
   final List<String> bought = List<String>.from(json["acquistati"]);
 
+  // Questo permette di visualizzare gli ingredienti già selezionati quando il widget viene costruito per la prima volta
   @override
   void initState() {
     super.initState();
@@ -55,27 +60,39 @@ class _IngredientListScreenState extends State<IngredientListScreen> {
       ),
       backgroundColor: kBlackColor,
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+        crossAxisAlignment: CrossAxisAlignment
+            .stretch, // Allinea gli elementi lungo l'asse trasversale (orizzontale)
         children: [
-          SizedBox(height: 20),
+          Divider(
+            color: kWhiteColor, // Colore del separatore
+            thickness: 2.0, // Spessore del separatore
+          ),
+          SizedBox(
+            height: 10,
+          ),
           Text(
-            'Acquistabili',
+            'Da acquistare',
             textAlign: TextAlign.center,
             style: TextStyle(
                 fontSize: 20, fontWeight: FontWeight.bold, color: kWhiteColor),
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: ingredients.length,
+              itemCount: ingredients
+                  .length, // Numero totale di ingredienti nella lista
               itemBuilder: (context, index) {
-                final ingredient = ingredients[index];
-                final isBought = bought.contains(ingredient);
+                final ingredient = ingredients[
+                    index]; // Ottiene l'ingrediente corrente dall'elenco
+                final isBought = bought.contains(
+                    ingredient); // Verifica se l'ingrediente è già stato acquistato
+
                 return Theme(
+                  // Applica un tema personalizzato per il widget
                   data: Theme.of(context).copyWith(
                     unselectedWidgetColor: kWhiteColor,
                     checkboxTheme: CheckboxThemeData(
                       side: BorderSide(
-                        color: kOrangeColor,
+                        color: kOrangeColor, // Colore del bordo del checkbox
                       ),
                     ),
                   ),
@@ -87,63 +104,108 @@ class _IngredientListScreenState extends State<IngredientListScreen> {
                             ingredient,
                             style: TextStyle(color: kWhiteColor),
                           ),
-                          controlAffinity: ListTileControlAffinity.leading,
-                          value: isBought,
+                          controlAffinity: ListTileControlAffinity
+                              .leading, // Posizione del checkbox rispetto al testo
+                          value:
+                              isBought, // Valore del checkbox (se selezionato o meno)
                           onChanged: (bool? value) {
                             setState(() {
                               if (value != null) {
                                 if (value) {
-                                  bought.add(ingredient);
-                                  ingredients.remove(ingredient);
+                                  bought.add(
+                                      ingredient); // Aggiunge l'ingrediente agli acquistati
+                                  ingredients.remove(
+                                      ingredient); // Rimuove l'ingrediente dagli acquistabili
                                 } else {
-                                  bought.remove(ingredient);
-                                  ingredients.add(ingredient);
+                                  bought.remove(
+                                      ingredient); // Rimuove l'ingrediente dagli acquistati
+                                  ingredients.add(
+                                      ingredient); // Aggiunge l'ingrediente agli acquistabili
                                 }
-                                _updateJson();
+                                _updateJson(); // Aggiorna i dati JSON dopo ogni modifica
                               }
                             });
                           },
                         ),
                       ),
+                      // Se l'ingrediente non è stato acquistato, mostra l'icona per eliminare l'ingrediente
+                      if (!isBought)
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          color: kOrangeColor,
+                          onPressed: () {
+                            setState(() {
+                              ingredients.remove(
+                                  ingredient); // Rimuove l'ingrediente dagli acquistabili
+                              _updateJson(); // Aggiorna i dati JSON dopo la rimozione
+                            });
+                          },
+                        ),
                     ],
                   ),
                 );
               },
             ),
           ),
-          SizedBox(height: 20),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: 20.0),
             child: Divider(
-              color: kWhiteColor,
-              thickness: 2.0,
+              color: kWhiteColor, // Colore del separatore
+              thickness: 2.0, // Spessore del separatore
             ),
           ),
-          SizedBox(height: 20),
+          SizedBox(
+            height: 10,
+          ),
           Text(
-            'Acquistati',
-            textAlign: TextAlign.center,
+            'Acquistati', // Testo per la sezione degli ingredienti acquistati
+            textAlign: TextAlign.center, // Allinea il testo al centro
             style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.bold, color: kWhiteColor),
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: kWhiteColor), // Stile del testo
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: bought.length,
+              itemCount: bought
+                  .length, // Numero di elementi nella lista degli ingredienti acquistati
               itemBuilder: (context, index) {
-                final ingredient = bought[index];
+                final ingredient = bought[index]; // Ingrediente corrente
                 return ListTile(
-                  title: Text(
-                    ingredient,
-                    style: TextStyle(color: kWhiteColor),
+                  title: Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          ingredient,
+                          style:
+                              TextStyle(color: kWhiteColor), // Stile del testo
+                        ),
+                      ),
+                      IconButton(
+                        icon: Icon(
+                            Icons.delete), // Icona per eliminare l'ingrediente
+                        color: kOrangeColor, // Colore dell'icona
+                        onPressed: () {
+                          setState(() {
+                            bought.remove(
+                                ingredient); // Rimuove l'ingrediente dalla lista degli acquistati
+                            _updateJson(); // Aggiorna i dati JSON
+                          });
+                        },
+                      ),
+                    ],
                   ),
                   leading: IconButton(
-                    icon: Icon(Icons.check_box),
-                    color: kOrangeColor,
+                    icon: Icon(Icons
+                        .check_box), // Icona per spostare l'ingrediente negli acquistabili
+                    color: kOrangeColor, // Colore dell'icona
                     onPressed: () {
                       setState(() {
-                        bought.remove(ingredient);
-                        ingredients.add(ingredient);
-                        _updateJson();
+                        bought.remove(
+                            ingredient); // Rimuove l'ingrediente dalla lista degli acquistati
+                        ingredients.add(
+                            ingredient); // Aggiunge l'ingrediente agli acquistabili
+                        _updateJson(); // Aggiorna i dati JSON
                       });
                     },
                   ),
@@ -154,47 +216,47 @@ class _IngredientListScreenState extends State<IngredientListScreen> {
         ],
       ),
       floatingActionButton: SizedBox(
-        width: 70,
-        height: 70,
+        width: 70, // Larghezza del pulsante
+        height: 70, // Altezza del pulsante
         child: FloatingActionButton(
           onPressed: () {
             Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (context) => ModifyListScreen(
+                  // Naviga alla schermata di modifica della lista
                   onOk: (ing) {
                     setState(() {
-                      ingredients.addAll(ing);
+                      ingredients.addAll(
+                          ing); // Aggiunge gli ingredienti dalla modifica alla lista
                     });
-                    Navigator.pop(context);
+                    Navigator.pop(context); // Chiude la schermata di modifica
                   },
                 ),
               ),
             );
           },
           child: Transform.scale(
-            scale: 1.5,
-            child: Icon(Icons.edit),
+            scale: 1.5, // Scala dell'icona del pulsante
+            child: Icon(Icons.edit), // Icona del pulsante
           ),
           shape: CircleBorder(
-            side: BorderSide(color: kOrangeColor, width: 2.0),
+            side: BorderSide(
+                color: kOrangeColor, width: 2.0), // Bordo del pulsante
           ),
-          backgroundColor: kDarkGreyColor,
-          foregroundColor: kOrangeColor,
+          backgroundColor: kDarkGreyColor, // Colore di sfondo del pulsante
+          foregroundColor: kOrangeColor, // Colore dell'icona del pulsante
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.endFloat, // Posizione del pulsante
     );
   }
 
   void _updateJson() {
-    json['acquistabili'] = List<String>.from(ingredients);
-    json['acquistati'] = List<String>.from(bought);
+    json['acquistabili'] = List<String>.from(
+        ingredients); // Aggiorna gli ingredienti acquistabili nei dati JSON
+    json['acquistati'] = List<String>.from(
+        bought); // Aggiorna gli ingredienti acquistati nei dati JSON
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: IngredientListScreen(),
-  ));
 }
